6489aef8d077e7efeb531a85ba1d3dcd2a6190ae
LJ <joi@vcu.edu>
2023-09-23 14:39:50 -0400

    Initial commit

---
+++ R/mean.R
@@ -0,0 +1,18 @@
+#' mean
+#'
+#' This function will ...
+#'
+#' @param x a vector of numbers that we will use to take mean
+#' @param s The lower trim bound
+#' @param l The upper trim bound
+#' @return Returns a trimmed mean
+#' @author Leean Jo
+#' @examples
+#' mean(1:5)
+#'
+#'
+#' @export
+
+mean <- function(x){
+  mean(x)
+}
---
+++ R/p.R
@@ -0,0 +1,12 @@
+#' p
+#' The second section is a longer description of the function.
+#' This can go on for multiple lines.
+#'
+#' @param x numeric vector
+#' @export
+
+p <- function(a,b,c) {
+
+  c^2 = a^2 + b^2
+
+  }
